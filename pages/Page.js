92058911fc7f62c3c48bf836d44038003b4b57b0
Alp Koseoglu <alppkoseoglu@gmail.com>
2020-10-22 16:43:11 +0300

    Case sensitivity on

---
+++ pages/Page.js
@@ -0,0 +1,24 @@
+import Navigation from "../components/Navigation"
+import Head from "next/head"
+
+const Page = ({children}) => {
+    return (
+        
+        <div className="page">
+            <Head>
+                <title>MovieUp</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <script src="https://kit.fontawesome.com/4b6c7a7fa2.js" crossOrigin="anonymous"></script>
+                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+                <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'></link>
+            </Head>
+            <Navigation />
+            <main>
+                {children}
+            </main>
+        </div>
+        
+    )
+}
+
+export default Page

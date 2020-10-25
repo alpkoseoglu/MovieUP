7cdec7f96a9b22d7064ebfdeeef77c3196af0965
Alp Koseoglu <alppkoseoglu@gmail.com>
2020-10-25 19:06:27 +0300

    Favicon desn't work but it's there.

--- pages/Page.js
+++ pages/Page.js
@@ -1,23 +1,32 @@
 import Navigation from "../components/Navigation"
 import Head from "next/head"
 
-const Page = ({children}) => {
+const Page = ({ children }) => {
     return (
-        
         <div className="page">
             <Head>
                 <title>MovieUp</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <script src="https://kit.fontawesome.com/4b6c7a7fa2.js" crossOrigin="anonymous"></script>
-                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
-                <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'></link>
+                <link rel="favicon" href="/favicon.ico"></link>
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                />
+                <script
+                    src="https://kit.fontawesome.com/4b6c7a7fa2.js"
+                    crossOrigin="anonymous"
+                ></script>
+                <link
+                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                    rel="stylesheet"
+                ></link>
+                <link
+                    href="https://fonts.googleapis.com/css?family=Poppins"
+                    rel="stylesheet"
+                ></link>
             </Head>
             <Navigation />
-            <main>
-                {children}
-            </main>
+            <main>{children}</main>
         </div>
-        
     )
 }
 
